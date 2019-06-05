0f4eab6be3aa56c17602cdee0a7d6b59fa8957db
Fábio Costa <36956547+fabiocosta88@users.noreply.github.com>
2019-06-05 08:31:00 -0300

    Pasta view "atualizada"

--- VIEW/telaCaixa.php
+++ VIEW/telaCaixa.php
@@ -1,5 +1,9 @@
 <?php
 require_once("../CONTROLLER/Outros/cons_quant_prod.php");
+require_once("../MODEL/PHP files/DAOCadastroGeral.php");
+require_once("../CONTROLLER/Outros/CheckPagina.php");
+
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -8,13 +12,18 @@ require_once("../CONTROLLER/Outros/cons_quant_prod.php");
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width-device-width, initial-scale=1">
-    <title>Game Store</title>
+
+    <title>GameStore - Caixa</title>
+
     <script src="https://code.jquery.com/jquery-3.1.1.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.quicksearch/2.3.1/jquery.quicksearch.js"></script>
     <script src="https://ajax.googlepis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
     <script src="_js/bootstrap.js"></script>
     <script src="_js/interativos.js"></script>
+    <script src="_js/dados.js"></script>
+
     <link rel="stylesheet" href="_css/bootstrap.css">
     <link rel="stylesheet" href="_css/variations.css">
     <link rel="stylesheet" href="_css/forms.css">
@@ -22,7 +31,22 @@ require_once("../CONTROLLER/Outros/cons_quant_prod.php");
 
 <body>
     <div class="linha linhaUser2">
-        <div class="contentLine">Funcionario Atual <a class="logOutbtn" onclick="modalShowOut()">Log out</a></div>
+        <div class="contentLine">
+            <a style="text-align: Left"><strong>Voltar</strong></a>
+            <a style="text-align: center"><strong>Caixa!</strong></a>
+            <?php
+            if(isset($_SESSION['email'])){
+                $nome = new DAOCadastroGeral();
+                $nome->Email = $_SESSION['email'];
+                echo "Bem-Vindo ".$nome->readName();
+            }
+            else
+            {
+                echo "Funcionario Atual";
+            }
+            ?>
+            <a class="logOutbtn" onclick="modalShowOut()">Log out</a>
+        </div>
     </div>
     <section class="content-site3">
         <div class="blocoPainel">
@@ -66,25 +90,25 @@ require_once("../CONTROLLER/Outros/cons_quant_prod.php");
                                     <th>Quantidade</th>
                                 </tr>
                             </thead>
-                            <tbody>
-                                <tr>
-                                    <td>1002</td>
-                                    <td>Batman Arkhan Origins</td>
-                                    <td>R$ 89,90</td>
-                                    <td>1</td>
-                                </tr>
-                                <tr>
-                                    <td>1004</td>
-                                    <td>A volta dos que não foram</td>
-                                    <td>R$ 1209,90</td>
-                                    <td>1</td>
-                                </tr>
-                                <tr>
-                                    <td>1005</td>
-                                    <td>Kingdom Hearts III</td>
-                                    <td>R$ 140,71</td>
-                                    <td>1</td>
-                                </tr>
+                            <tbody id="listProd">
+<!--                                <tr>-->
+<!--                                    <td>1002</td>-->
+<!--                                    <td>Batman Arkhan Origins</td>-->
+<!--                                    <td>R$ 89,90</td>-->
+<!--                                    <td>1</td>-->
+<!--                                </tr>-->
+<!--                                <tr>-->
+<!--                                    <td>1004</td>-->
+<!--                                    <td>A volta dos que não foram</td>-->
+<!--                                    <td>R$ 1209,90</td>-->
+<!--                                    <td>1</td>-->
+<!--                                </tr>-->
+<!--                                <tr>-->
+<!--                                    <td>1005</td>-->
+<!--                                    <td>Kingdom Hearts III</td>-->
+<!--                                    <td>R$ 140,71</td>-->
+<!--                                    <td>1</td>-->
+<!--                                </tr>-->
                             </tbody>
                         </table>
                     </div>
@@ -94,7 +118,7 @@ require_once("../CONTROLLER/Outros/cons_quant_prod.php");
         <div class="pagar">
             <div class="caixa caixa1">
                 <h4 class="tx1 tp">R$ TOTAL A PAGAR</h4>
-                <h1 class="tpp">1.374,61</h1>
+                <h1 class="tpp"><label id="totalaPagar">00,00</label></h1>
             </div>
             <div class="caixa caixa2">
                 <h4 class="tx1 vp">R$ VALOR PAGO</h4>
@@ -116,7 +140,7 @@ require_once("../CONTROLLER/Outros/cons_quant_prod.php");
         <div class="container">
             <div class="row">
                 <div class="col-xs-12">
-                    <p class="text-center">Copyright &copy; 2019 - by CatStorm</p>
+                    <p class="text-center">Copyright &copy; 2019 - by KatStorm</p>
                 </div>
             </div>
         </div>
@@ -129,7 +153,9 @@ require_once("../CONTROLLER/Outros/cons_quant_prod.php");
         <div class="modal-content">
             <div id="close" class="close" onclick="modalCloseOut()">+</div>
             <form>
-                <p class="text-center">Deseja Sair de Sua Seção?</p> <a href="" class="btn btn-danger">Sim</a> <a onclick="modalCloseOut()" class="btn btn-dark">Cancelar</a>
+                <p class="text-center">Deseja Sair de Sua Seção?</p>
+                <a href="../CONTROLLER/Outros/Logout.php" class="btn btn-danger">Sim</a>
+                <a onclick="modalCloseOut()" class="btn btn-primary">Cancelar</a>
             </form>
         </div>
     </div>
@@ -151,11 +177,12 @@ require_once("../CONTROLLER/Outros/cons_quant_prod.php");
                                     <th class="actions">Ações</th>
                                 </tr>
                             </thead>
-                            <tbody> <?php impListaProdutos("../MODEL/PHP Files/DAOProdutos.php"); ?> </tbody>
+                            <tbody>
+                            <?php impListaProdutos("../MODEL/PHP Files/DAOProdutos.php","../MODEL/PHP Files/DAOMovimento_Geral.php"); ?>
+                            </tbody>
                         </table>
                         <script>
                             $('input#txt_consulta').quicksearch('table#tabela tbody tr');
-
                         </script>
                     </div>
                 </div>
